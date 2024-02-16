08fb9787b2c09ccdafdb61a9026c0eb2c37df9a8
Karan <kstkaranthakur03@gmail.com>
2024-02-17 04:07:44 +0530

    generalized error response classes

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+
+const app = express();
+
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//now we need to handle how different kind of data we can receive it could be anything like from url in json etc
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
---
+++ utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {//need to standardize in way which error is sent
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)//provide context related which stack trace needed to be done
+        }
+
+    }
+
+}
